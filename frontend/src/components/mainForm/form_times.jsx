0843375417a6dfc5e38ec3222a094a202b3b8df1
Jordan Ackerman <jork41989@gmail.com>
2020-04-03 23:54:32 -0400

    writing logic for orinizing dates

--- frontend/src/components/mainForm/form_times.jsx
+++ frontend/src/components/mainForm/form_times.jsx
@@ -13,7 +13,7 @@ class FormTimes extends React.Component {
 
   dateCheck(){
     let fullDate = new Date()
-    let day = fullDate.getDate()
+    let dayNow = fullDate.getDate()
     let month = fullDate.getMonth() + 1
     let year = fullDate.getFullYear()
     let dates = Object.keys(this.props.times)
@@ -21,13 +21,19 @@ class FormTimes extends React.Component {
     if (dates){
       dates.forEach(day =>{
         let localyear = parseInt(day.slice(0, 4))
-        let localMonth = parseInt(day.slice(5,6))
+        let localMonth = parseInt(day.slice(5,7))
+        let localDay = parseInt(day.slice(8,10))
         if (localyear >= year){
-          console.log(localMonth)
+          if (localMonth >= month){
+            if (localDay >= dayNow){
+              organized.push(day)
+            }
+          }
+          
         }
       })
     }
-
+    return organized
   }
 
 
